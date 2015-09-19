37e16bab1a50b7aa52210054cff0efc1b25e9673
Daniel Stiner <danstiner@gmail.com>
2015-09-18 20:25:37 -0700

    Test zero is zero

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -6,6 +6,14 @@ module StringCalculator
     ) where
 
 import Test.Framework
+import Test.HUnit
+
+data Calculation = Error String | Valid Integer deriving (Eq, Show)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+calculate :: String -> Calculation
+calculate _ = Valid 0
+
+test_zero_is_zero = Valid 0 @=? calculate "0"
