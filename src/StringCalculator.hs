67f57047776002923b2d869b2086c6201ad7ac99
Daniel Stiner <danstiner@gmail.com>
2015-09-18 20:37:32 -0700

    Test helper for calculate function

--- src/StringCalculator.hs
+++ src/StringCalculator.hs
@@ -16,4 +16,9 @@ someFunc = putStrLn "someFunc"
 calculate :: String -> Calculation
 calculate _ = Valid 0
 
-test_zero_is_zero = Valid 0 @=? calculate "0"
+calculate' :: String -> Integer
+calculate' input = case calculate input of
+    Valid result -> result
+    Error reason -> error reason
+
+test_zero_is_zero = 0 @=? calculate' "0"
